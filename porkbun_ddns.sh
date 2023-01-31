768a130ac4fba26673bea173b918e494514e82cf
jjc5891 <james.chrzanowski@gmail.com>
2023-01-31 00:51:05 +0000

    Updates to comments for ifconfig

--- porkbun_ddns.sh
+++ porkbun_ddns.sh
@@ -13,12 +13,12 @@ record=
 if [ -z "$apikey" ] || [ -z "$apisec" ]; then echo "api key and secret code must be provided" && exit; fi
 if [ -z "$domain" ] || [ -z "$record" ]; then echo "domain and record to update must be specified" && exit; fi
 
-# ping porkbun api to get our current ip address
+# use ifconfig to get our current ip address, may have to update your interface (bce0) to match your setup
 ourip=$(ifconfig bce0 | awk '$1 == "inet" {print $2}')
-if [ -z "$ourip" ]; then echo "could not get our external ip address from ifconfig -- please check internet connectivity and api credentials" && exit; fi
+if [ -z "$ourip" ]; then echo "could not get our WAN ip address from ifconfig" && exit; fi
 
 # update dns record
-# make sure the A record exists first
+# make sure the A record exists first!
 curl -X POST "https://porkbun.com/api/json/v3/dns/editByNameType/$domain/A/$record" \
   -H "Content-Type: application/json" \
   --data "{ \"apikey\": \"$apikey\", \"secretapikey\": \"$apisec\", \"content\": \"$ourip\", \"ttl\": \"600\" }"
